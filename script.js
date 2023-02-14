fae8b50eff4c7d042ddece7a3d1664b9c5580fca
Daniel Laschanzky <dlaschanzky2@huskers.unl.edu>
2023-02-14 16:29:31 -0600

    created functions for user input checkWinner

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const options = ["rock", "paper", "scissors"]
 
-const x = getComputerChoice()
+const ComputerSelection = getComputerChoice()
+
 function getComputerChoice(){
     const computerChoice = options[Math.floor(Math.random() * options.length)]
     return computerChoice
 }
+
+const UserSelection = getUserChoice()
+
 function getUserChoice(){
-    userChoice = prompt("Choose your weapon: rock, paper, or scissors")
+    const userChoice = prompt("Choose your weapon: rock, paper, or scissors")
     return userChoice
 }
-const y = getUserChoice()
-console.log(y)
-console.log(x)
-if y == "rock", x == "paper",
-    
+function playRound(UserSelection, ComputerSelection){
+
+}
+function checkWinner(){
+    if (UserSelection == ComputerSelection){
+        return "Tie";
+    }
+    else if(
+        (UserSelection == "rock" && ComputerSelection == "scissors") ||
+        (UserSelection == "paper" && ComputerSelection == "rock") ||
+        (UserSelection == "scissors" && ComputerSelection == "paper"))
+        {
+            return "You win!"
+    }
+    else if(
+        (UserSelection == "rock" && ComputerSelection == "paper") ||
+        (UserSelection == "paper" && ComputerSelection == "scissors") ||
+        (UserSelection == "scissors" && ComputerSelection == "rock"))
+        {
+            return "You lose :("
+        }
+}
+
+console.log(checkWinner())
+
+console.log(ComputerSelection)
+
+console.log(UserSelection)
